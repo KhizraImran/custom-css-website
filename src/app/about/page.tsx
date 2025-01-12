6892a8edff1c68b0bd47c8cfba7244499eda0516
Khizra Imran <161931364+KhizraImran@users.noreply.github.com>
2025-01-12 11:45:27 -0800

    Update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className='about-content'>
       <h2 className='fade-in'>About Us</h2>
       <p>
-Welcome to Delicate Earrings, where elegance meets artistry in every design. Our curated collection celebrates the timeless beauty of earrings, each piece meticulously crafted to bring out the essence of sophistication and charm. From understated studs to bold statement pieces, our range is designed to suit every occasion and style. Whether you're looking to add a touch of glamour to your everyday look or searching for that perfect accessory to complete your evening ensemble, Delicate Earrings has something special just for you. Indulge in the luxury of fine craftsmanship and elevate your style with our exquisite pieces.</p>
+Welcome to Delicate Earrings, where elegance meets artistry in every design. Our curated collection celebrates the timeless beauty of earrings, each piece meticulously crafted to bring out the essence of sophistication and charm. From understated studs to bold statement pieces, our range is designed to suit every occasion and style. Whether you&apos; re looking to add a touch of glamour to your everyday look or searching for that perfect accessory to complete your evening ensemble, Delicate Earrings has something special just for you. Indulge in the luxury of fine craftsmanship and elevate your style with our exquisite pieces.</p>
       </div>
       </section>
     </div>
