be8e5b16a649440d98901cbff29f821ce9b4bd72
Khizra Imran <khizraimran26@gmail.com>
2025-01-12 03:59:28 -0800

    Update

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <section className='hero' style={{backgroundImage:"url('/main img.jpeg')"}}>
+      <section className='hero' style={{backgroundImage:"url('/main img.jpeg ')"}}>
        <div>
         <h1 className='text-in'>Welcome to Delicate Earrings</h1>
         <p>"Discover Elegant Earrings That Define Your Style <br /> Perfect for Every Occasion!" </p>
